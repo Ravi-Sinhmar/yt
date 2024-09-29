6a1c58b97b901eab62b974f9fc1eede8645d8476
Ravi-Sinhmar <engineerfuture82@gmail.com>
2024-09-29 09:44:46 +0530

    let's check

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
+const scrapeLogic  = require("./scrapeLogic");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", (req, res) => {
+app.get("/data", (req, res) => {
   scrapeLogic(res);
 });
 
--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,12 +1,15 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const scrapeLogic = async (res) => {
+// Getting Users Bio/Profile Details
+const profile = async (res) => {
+
   const browser = await puppeteer.launch({
+    headless: false,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
-      "--single-process",
+      // "--single-process",
       "--no-zygote",
     ],
     executablePath:
@@ -14,38 +17,49 @@ const scrapeLogic = async (res) => {
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
-  try {
-    const page = await browser.newPage();
+  const page = await browser.newPage();
+  const iPhone = puppeteer.KnownDevices['iPhone 14 Pro Max'];
+  await page.emulate(iPhone);
 
-    await page.goto("https://developer.chrome.com/");
+  await page.goto("https://www.instagram.com" ,  {
+    "waitUntil": "networkidle0",
+    "timeout": "300000"
+  }); // Replace with the actual URL
+  
+  // Wait for the article element
+  const article = await page.waitForSelector('article');
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
+  
+  // Function to handle login
+  const data = await article.evaluate(async (article) => {
+    let finalData = null;
+    // Delay function
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    let count = 0;
+    while (true) {
+      const allButtons = article.querySelectorAll('button , a');
+      console.log('allButtons', allButtons);
+      
+      const loginLinks = Array.from(allButtons).filter(link => 
+        link.textContent.toLowerCase().replace(" ", "").includes('login')
+      );
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+      if (loginLinks.length > 0 || count > 10) {
+        if (loginLinks.length > 0) {
+          finalData = loginLinks[0].innerText;
+          console.log("Login link found: ", loginLinks[0]);
+        }
+        break;
+      } 
 
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+      count++;
+      await delay(500); // Await the delay
+    }
 
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
-  } catch (e) {
-    console.error(e);
-    res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
-    await browser.close();
-  }
-};
-
-module.exports = { scrapeLogic };
+    return finalData;
+  });
+  
+res.send("Data",data);
+ return data;
+}
+module.exports = profile;
